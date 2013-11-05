ed98a09b9e162e7805238cf7f9f4d19884a8f6e2
Gabe Fierro <gtfierro225@gmail.com>
2013-11-05 13:22:55 -0800

    add insertTexts script

---
+++ insertTexts.py
@@ -0,0 +1,44 @@
+import os
+import glob
+import rethinkdb as r
+from pyocr import tesseract
+import wand.image
+from PIL import Image
+
+r.connect().repl()
+db = r.db('invalid')
+table = db.table('invalid')
+
+def convertPDF(filename):
+    img = wand.image.Image(filename=filename, resolution=200)
+    img.compression = 99
+    img.save(filename=filename+'.jpg')
+    return filename+'.jpg' 
+
+def getText(filename):
+    img = Image.open(filename)
+    text = tesseract.image_to_string(img)
+    return text
+
+def getNumberFromFilename(filename):
+    return filename.split('/')[-1].split('.')[0]
+
+def getFileNames():
+    pdfs = glob.glob('pdf_files/*.pdf')
+    print pdfs
+    return pdfs
+
+def run():
+    for filename in getFileNames():
+        number = getNumberFromFilename(filename)
+        convertPDF(filename)
+        print filename
+        for i, jpg in enumerate(glob.glob(filename+'-[0-9]*.jpg')):
+            text = getText(jpg)
+            table.insert({'filename': filename,
+                          'sequence': i,
+                          'number': number,
+                          'text': text}).run()
+
+if __name__=='__main__':
+    run()
