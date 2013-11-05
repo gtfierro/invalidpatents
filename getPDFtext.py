b0f0c217ab6787699dc80ed65f142788a49248b3
Gabe Fierro <gtfierro225@gmail.com>
2013-11-05 10:32:18 -0800

    add files

---
+++ getPDFtext.py
@@ -0,0 +1,39 @@
+import sys
+import os
+from pyocr import tesseract
+import wand.image
+from PIL import Image
+from selenium import webdriver
+
+def convertPDF(filename):
+    img = wand.image.Image(filename=filename, resolution=200)
+    img.compression = 99
+    img.save(filename=filename+'.jpg')
+    return filename+'.jpg' 
+
+def getText(filename):
+    img = Image.open(filename)
+    text = tesseract.image_to_string(img)
+    return text
+
+
+def downloadLastN(n)
+    fp = webdriver.FirefoxProfile()
+    fp.set_preference("browser.download.folderList", 2)
+    fp.set_preference("browser.download.manager.showWhenStarting", True)
+    fp.set_preference("browser.download.dir", os.getcwd())
+    fp.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/pdf")
+    fp.set_preference("pdfjs.disabled", True)
+    fp.update_preferences()
+    browser = webdriver.Firefox(firefox_profile=fp)
+    browser.get('http://e-foia.uspto.gov/Foia/DispatchBPAIServlet?RetrieveRecent='+n)
+
+    links = browser.find_elements_by_tag_name('a')
+    names = [link.get_attribute('name') if link else '' for link in links]
+    documents = [doc if doc.isdigit() else '' for doc in names] 
+    documents = filter(lambda x: x, documents)
+    for document in documents:
+        browser.get('http://e-foia.uspto.gov/Foia/DispatchBPAIServlet?RetrieveRecent=30')
+        downloadlink = browser.find_element_by_partial_link_text(document)
+        downloadlink.click()
+        os.rename('ReterivePdf',document+'.pdf')
