143c630a9b3b8ac96963cc56e072d746bb5894d0
Gabe Fierro <gtfierro225@gmail.com>
2013-11-05 11:39:31 -0800

    guarantee correct file rename on download

--- getPDFs.py
+++ getPDFs.py
@@ -1,23 +1,18 @@
 import sys
 import os
-from pyocr import tesseract
-import wand.image
-from PIL import Image
 from selenium import webdriver
+import time
+import glob
 
-def convertPDF(filename):
-    img = wand.image.Image(filename=filename, resolution=200)
-    img.compression = 99
-    img.save(filename=filename+'.jpg')
-    return filename+'.jpg' 
+def spinWaitOnDownload(document):
+    while 1:
+        retrieved = glob.glob('pdf_files/ReterivePdf*')
+        if not retrieved:
+            break
+        os.rename(retrieved[0],'pdf_files/'+document+'.pdf')
+        time.sleep(1)
 
-def getText(filename):
-    img = Image.open(filename)
-    text = tesseract.image_to_string(img)
-    return text
-
-
-def downloadLastN(n)
+def downloadLastN(n):
     fp = webdriver.FirefoxProfile()
     fp.set_preference("browser.download.folderList", 2)
     fp.set_preference("browser.download.manager.showWhenStarting", True)
@@ -26,7 +21,7 @@ def downloadLastN(n)
     fp.set_preference("pdfjs.disabled", True)
     fp.update_preferences()
     browser = webdriver.Firefox(firefox_profile=fp)
-    browser.get('http://e-foia.uspto.gov/Foia/DispatchBPAIServlet?RetrieveRecent='+n)
+    browser.get('http://e-foia.uspto.gov/Foia/DispatchBPAIServlet?RetrieveRecent='+str(n))
 
     links = browser.find_elements_by_tag_name('a')
     names = [link.get_attribute('name') if link else '' for link in links]
@@ -36,16 +31,16 @@ def downloadLastN(n)
         browser.get('http://e-foia.uspto.gov/Foia/DispatchBPAIServlet?RetrieveRecent=30')
         downloadlink = browser.find_element_by_partial_link_text(document)
         downloadlink.click()
-        os.rename('ReterivePdf',document+'.pdf')
-
-
+        spinWaitOnDownload(document)
 
 if __name__=='__main__':
     if len(sys.argv) < 2:
         print 'Usage: python getPDFs.py <number-of-days-to-download>'
+        sys.exit(0)
     num = sys.argv[1]
     if not num.isdigit():
         print 'Usage: python getPDFs.py <number-of-days-to-download>'
+        sys.exit(0)
     if not os.path.exists('pdf_files'):
         os.mkdir('pdf_files')
     downloadLastN(int(num))
